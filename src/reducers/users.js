0b2c07319de4c9a035db9f2526d3809e7ae2ffaa
shgysd <shgysd@gmail.com>
2019-02-23 07:10:48 +0900

    Refactor reducers/users using ESLint

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -6,14 +6,14 @@ import {
   SIGNUP,
   SUCCESS_LOGIN,
   SUCCESS_SIGNOUT,
-  SUCCESS_SIGNUP
-} from '../consistants/actionTypes'
+  SUCCESS_SIGNUP,
+} from '../consistants/actionTypes';
 
 const initialState = {
   email: '',
   password: '',
   uid: null,
-  isLoading: false
+  isLoading: false,
 };
 
 const reducer = (state = initialState, action) => {
@@ -21,33 +21,33 @@ const reducer = (state = initialState, action) => {
     case CHANGE_LOGIN_EMAIL:
       return {
         ...state,
-        email: action.email
-      }
+        email: action.email,
+      };
     case CHANGE_LOGIN_PASSWORD:
       return {
         ...state,
-        password: action.password
-      }
+        password: action.password,
+      };
     case LOGIN:
       return {
         ...state,
         email: action.email,
         password: action.password,
-        isLoading: true
-      }
+        isLoading: true,
+      };
     case SIGNOUT:
       return {
         ...state,
         uid: null,
-        isLoading: false
-      }
+        isLoading: false,
+      };
     case SIGNUP:
       return {
         ...state,
         email: action.email,
         password: action.password,
-        isLoading: false
-      }
+        isLoading: false,
+      };
     case SUCCESS_LOGIN:
       return {
         ...state,
@@ -55,26 +55,26 @@ const reducer = (state = initialState, action) => {
         isLoading: false,
         email: '',
         password: '',
-      }
+      };
     case SUCCESS_SIGNOUT:
       return {
         ...state,
         uid: null,
         isLoading: false,
         email: '',
-        password: ''
-      }
+        password: '',
+      };
     case SUCCESS_SIGNUP:
       return {
         ...state,
         uid: action.uid,
         isLoading: false,
         email: '',
-        password: ''
-      }
+        password: '',
+      };
     default:
       return state;
   }
-}
+};
 
 export default reducer;
